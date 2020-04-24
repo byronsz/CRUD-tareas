c520ab1c9725ce91c240255d61f9bcbc0d28f2b3
Simon Zapata <simonbze@gmail.com>
2020-04-24 12:12:12 +0200

    Carpeta docs creada

---
+++ docs/precache-manifest.4e364af78d9932b7120a31e4f0349567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162baae67a31d5805f58206ffb618e08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bd0f0d6ec68773b2d03",
+    "url": "/static/css/main.610e8de2.chunk.css"
+  },
+  {
+    "revision": "17dd3b1da38a3e648fe8",
+    "url": "/static/js/2.8ec938c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ec938c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd0f0d6ec68773b2d03",
+    "url": "/static/js/main.0427cec3.chunk.js"
+  },
+  {
+    "revision": "c7cab0e691400c070e9f",
+    "url": "/static/js/runtime-main.f13a553e.js"
+  }
+]);
